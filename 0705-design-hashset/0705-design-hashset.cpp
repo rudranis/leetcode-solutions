9f1446820e7aea1bc8fb132b1de35d8051425ceb
rudranishinde <146566511+rudranis@users.noreply.github.com>
2026-05-08 11:28:47 +0530

    Time: 9 ms (81.98%), Space: 50.3 MB (51.73%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,21 @@
+class MyHashSet {
+public:
+    
+    vector<bool> hash;
+
+    MyHashSet() {
+        hash.resize(1000001, false);
+    }
+    
+    void add(int key) {
+        hash[key] = true;
+    }
+    
+    void remove(int key) {
+        hash[key] = false;
+    }
+    
+    bool contains(int key) {
+        return hash[key];
+    }
+};
