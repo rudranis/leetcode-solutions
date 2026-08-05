39550754ee3958a5898cb5d1ec279d9f9b2d7b61
rudranishinde <146566511+rudranis@users.noreply.github.com>
2026-08-05 08:29:11 +0530

    Time: 162 ms (82.41%), Space: 305.9 MB (68.97%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void dfs(int node,vector<vector<int>>&adj,vector<bool>&vis){
+        vis[node]=true;
+        for(int nei:adj[node]){
+            if(!vis[nei]){
+                dfs(nei,adj,vis);
+            }
+        }
+    }
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>>adj(n);
+        for(auto &edge:invocations){
+            adj[edge[0]].push_back(edge[1]);
+        }
+        vector<bool>suspicious(n,false);
+        dfs(k,adj,suspicious);
+        for(auto &edge:invocations){
+            int u=edge[0];
+            int v=edge[1];
+            if(!suspicious[u] && suspicious[v]){
+                vector<int>ans;
+                for(int i=0;i<n;i++){
+                    ans.push_back(i);
+                }
+                return ans;
+            }
+        }
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(!suspicious[i]){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
