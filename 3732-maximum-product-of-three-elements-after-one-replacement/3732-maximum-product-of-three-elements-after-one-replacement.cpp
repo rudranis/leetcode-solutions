dc365b52fc46059019358a27396fabf4f879b40e
rudranishinde <146566511+rudranis@users.noreply.github.com>
2026-08-28 18:35:18 +0530

    Time: 9 ms (62.51%), Space: 124.3 MB (44.62%) - LeetHub

---
+++ 3732-maximum-product-of-three-elements-after-one-replacement/3732-maximum-product-of-three-elements-after-one-replacement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maxProduct(vector<int>& nums) {
+        long long ans, max1 = -1, max2 = -1;
+        for(int i = 0; i < nums.size(); i++) {
+            nums[i] = abs(nums[i]);
+            if(nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if(nums[i] > max2) max2 = nums[i];
+        }
+        ans = max1 * max2 * 100000;
+        return ans;
+    }
+};
